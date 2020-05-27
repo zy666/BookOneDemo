8deb3e877d1d4f579c8a4fc7bda85bf02c58722f
周游 <2218803995@qq.com>
2020-05-27 23:12:05 +0800

    添加tablayout

--- bookOne/app/src/main/java/com/danny/bookone/fragment/BookFragment.java
+++ bookOne/app/src/main/java/com/danny/bookone/fragment/BookFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
@@ -30,7 +31,8 @@ import java.util.List;
 
 public class BookFragment extends BaseFragment {
     private List<String> arraylist = new ArrayList<>();
-    private FragmentBookBinding testBinding;
+    private FragmentBookBinding bookBinding;
+    private ArrayList<Fragment> mFragments;
 
     public BookFragment() {
         // Required empty public constructor
@@ -44,46 +46,52 @@ public class BookFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        testBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_book, container, false);
-        return testBinding.getRoot();
+        bookBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_book, container, false);
+        return bookBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        StringAdapter adapter = new StringAdapter(arraylist);
-        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
-            @Override
-            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
-                switch (position) {
-                    case 0:
-                        startActivity(HeroActivity.Companion.createIntent(getActivity()));//Android群英传
-                        break;
-                    case 1:
-                        startActivity(HighLightActivity.createIntent(getActivity()));//android进阶之光
-                        break;
-                    case 2:
-                        startActivity(AacActivity.createIntent(getActivity()));//Jetpack架构篇
-                        break;
-                    case 3:
-                        startActivity(HandlerActivity.createIntent(getActivity()));//handler线程篇
-                        break;
-                    case 4:
-                        startActivity(DataActivity.createIntent(getActivity()));//数据结构篇。hashMap
-                        break;
-                    case 5:
-                        startActivity(CompanyActivity.createIntent(getActivity()));//android基础组件篇。服务，广播，activity
-                        break;
-                    case 6:
-                        startActivity(GlideImgActivity.createIntent(getActivity()));//android基础组件篇。服务，广播，activity
-                        break;
-                }
-            }
-        });
+//        StringAdapter adapter = new StringAdapter(arraylist);
+//        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+//            @Override
+//            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+//                switch (position) {
+//                    case 0:
+//                        startActivity(HeroActivity.Companion.createIntent(getActivity()));//Android群英传
+//                        break;
+//                    case 1:
+//                        startActivity(HighLightActivity.createIntent(getActivity()));//android进阶之光
+//                        break;
+//                    case 2:
+//                        startActivity(AacActivity.createIntent(getActivity()));//Jetpack架构篇
+//                        break;
+//                    case 3:
+//                        startActivity(HandlerActivity.createIntent(getActivity()));//handler线程篇
+//                        break;
+//                    case 4:
+//                        startActivity(DataActivity.createIntent(getActivity()));//数据结构篇。hashMap
+//                        break;
+//                    case 5:
+//                        startActivity(CompanyActivity.createIntent(getActivity()));//android基础组件篇。服务，广播，activity
+//                        break;
+//                    case 6:
+//                        startActivity(GlideImgActivity.createIntent(getActivity()));//android基础组件篇。服务，广播，activity
+//                        break;
+//                }
+//            }
+//        });
+//
+//
+//        bookBinding.rcvChapter.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        bookBinding.rcvChapter.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+//        bookBinding.rcvChapter.setAdapter(adapter);
+        mFragments = new ArrayList<>();
+        mFragments.add(new HeroFragment());
+        mFragments.add(new HeroFragment());
+        bookBinding.tabLayout.setViewPager(bookBinding.bookViewPager, new String[]{"Android群英传", "Android进阶之光"}, getActivity(), mFragments);
 
-        testBinding.rcvChapter.setLayoutManager(new LinearLayoutManager(getActivity()));
-        testBinding.rcvChapter.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
-        testBinding.rcvChapter.setAdapter(adapter);
     }
 
     @Override
---
+++ bookOne/app/src/main/java/com/danny/bookone/fragment/HeroFragment.java
@@ -0,0 +1,105 @@
+package com.danny.bookone.fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.danny.bookone.Base.StringAdapter;
+import com.danny.bookone.R;
+import com.danny.bookone.aac.AacActivity;
+import com.danny.bookone.databinding.FragmentHeroBinding;
+import com.danny.bookone.highLight.HighLightActivity;
+import com.danny.bookone.img.GlideImgActivity;
+import com.danny.bookone.one.HandlerActivity;
+import com.danny.bookone.one.HeroActivity;
+import com.danny.bookone.one.OneActivity;
+import com.danny.bookone.one.ThreeActivity;
+import com.danny.bookone.other.CompanyActivity;
+import com.danny.bookone.other.DataActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeroFragment extends BaseFragment {
+    private List<String> arraylist = new ArrayList<>();
+    private FragmentHeroBinding heroBinding;
+    private ArrayList<Fragment> mFragments;
+
+    public HeroFragment() {
+        // Required empty public constructor
+    }
+
+    public static HeroFragment newInstance() {
+        HeroFragment fragment = new HeroFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        heroBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_hero, container, false);
+        return heroBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        StringAdapter adapter = new StringAdapter(arraylist);
+        adapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                switch (position) {
+                    case 0:
+                        startActivity(OneActivity.Companion.createIntent(getActivity()));
+                        break;
+                    case 1:
+                        Toast.makeText(getActivity(), "暂无重点，先跳过", Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        startActivity(ThreeActivity.Companion.createIntent(getActivity()));//Jetpack架构篇
+                        break;
+                    case 6:
+                        startActivity(GlideImgActivity.createIntent(getActivity()));//android基础组件篇。服务，广播，activity
+                        break;
+                }
+            }
+        });
+
+        heroBinding.rcvData.setLayoutManager(new LinearLayoutManager(getActivity()));
+        heroBinding.rcvData.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        heroBinding.rcvData.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onLazyData() {
+        initData();
+        Log.e("Test-->", "onLazyData");
+    }
+
+    private void initData() {
+        arraylist.add("第一章-体系与系统架构");
+        arraylist.add("第二章-开发工具");
+        arraylist.add("第三章-架构控件与自定义控件详解");
+        arraylist.add("第四章");
+        arraylist.add("第五章");
+        arraylist.add("第六章");
+        arraylist.add("第七章-Android动画机制与使用技巧");
+        arraylist.add("第八章");
+        arraylist.add("第九章");
+        arraylist.add("第十章");
+        arraylist.add("第十一章");
+        arraylist.add("第十二章");
+        arraylist.add("第十三章");
+    }
+}
